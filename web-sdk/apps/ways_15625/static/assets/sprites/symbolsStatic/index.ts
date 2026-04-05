749244ac11e64c15a18b5048a5d4f74bfa56e2e5
Hemant Yadav <hemant.yadav@zensar.com>
2026-04-05 18:22:09 +0530

    Add new sprite assets and update references to use new image and atlas files

--- web-sdk/apps/ways_15625/static/assets/sprites/symbolsStatic/index.ts
+++ web-sdk/apps/ways_15625/static/assets/sprites/symbolsStatic/index.ts
@@ -1,6 +1,9 @@
 import { createAsset } from 'pixi-svelte';
 
-import img from './sprites.png';
-import atlas from './sprites.json';
+// import img from './sprites.png';
+// import atlas from './sprites.json';
+
+import img from './sprites_new.png';
+import atlas from './sprites_new.json';
 
 export default createAsset({ img, atlas });
